40749e0f98116a1de129309ce8e3fcc88eee021d
c0de4un <code4un@yandex.ru>
2024-07-09 17:15:46 +0400

    Add dummy grid view comp

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,13 @@
 import Row from '../comps/layouts/Row.tsx';
 import Col from '../comps/layouts/Col.tsx';
+import GridView from '../comps/layouts/GridView.tsx';
 
 export default function Home() {
   return (
     <Col className={'w-full h-full p-5'}>
       <Row className={'w-full justify-center flex-wrap'}>
+        <GridView
+        />
       </Row>
     </Col>
   )
