52399ef5738bbc1833a96580cb2f687ddbd7cb38
c0cde4un <code4un@yandex.ru>
2024-07-09 19:20:05 +0400

    feat: Add product model

---
+++ src/models/Product.ts
@@ -0,0 +1,8 @@
+export default class Product {
+    public title: string = '';
+    public id: number = 0;
+    public uuid: string = '';
+    public shortDescription: string = '';
+    public fullDescription: string = '';
+    public images: string[] = [];
+}
