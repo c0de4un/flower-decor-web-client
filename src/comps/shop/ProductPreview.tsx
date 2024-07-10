17c8bbcaa1f4e7a6c7e118abec76ed01a70ceba1
c0de4un <code4un@yandex.ru>
2024-07-11 01:27:29 +0400

    feat: Add product price render

--- src/comps/shop/ProductPreview.tsx
+++ src/comps/shop/ProductPreview.tsx
@@ -26,14 +26,20 @@ export default function ProductPreview(props: ProductPreviewProps) {
           />
         </Row>
 
-        <Row className={'w-full justify-center mt-4'}>
+        <Row className={'mt-2 w-full justify-center'}>
+          <span className={'price'}>{props.product.price}&nbsp;</span><b>₽</b>
+        </Row>
+
+        <Row className={'w-full justify-center mt-4 px-5'}>
           <button
-            className={'select-none rounded-lg bg-amber-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-black shadow-md shadow-amber-500/20 transition-all hover:shadow-lg hover:shadow-amber-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'}
+            className={'w-full select-none rounded-lg bg-amber-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-black shadow-md shadow-amber-500/20 transition-all hover:shadow-lg hover:shadow-amber-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'}
           >
-            <img
-              src={add_to_cart}
-              alt={'add-to-cart'}
-            />
+            <Row className={'w-full justify-center'}>
+              <img
+                src={add_to_cart}
+                alt={'add-to-cart'}
+              />
+            </Row>
           </button>
         </Row>
       </Col>
