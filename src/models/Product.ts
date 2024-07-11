5528aa66bbdb39179bf1d2acb2ea562ccc6edbcd
c0de4un <code4un@yandex.ru>
2024-07-11 12:35:45 +0400

    fix: Change Product images type

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,7 +5,7 @@ export default class Product {
     public uuid: string = '';
     public shortDescription: string = '';
     public fullDescription: string = '';
-    public images: object[] = [];
+    public images: string[] = [];
 
     public constructor(
         title: string = '',
@@ -14,7 +14,7 @@ export default class Product {
         uuid: string = '',
         shortDescription: string = '',
         fullDescription: string = '',
-        images: object[] = []
+        images: string[] = []
     ) {
         this.title = title;
         this.price = price;
