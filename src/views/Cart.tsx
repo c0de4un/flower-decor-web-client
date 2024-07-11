3fb73d0c1adba3fbf67337647e8ee6b76709e406
c0de4un <code4un@yandex.ru>
2024-07-11 17:57:22 +0400

    feat: Add cart items fetching logic

--- src/views/Cart.tsx
+++ src/views/Cart.tsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/rootReducer.ts';
+import { CartState } from '../redux/cartReducer.ts';
 import Row from '../comps/layouts/Row.tsx';
 import Col from '../comps/layouts/Col.tsx';
 import UmbrellaSpinner from '../comps/spinners/UmbrellaSpinner.tsx';
 import { getRandom } from '../math/Random.ts';
+import CartItem from '../models/CartItem.ts';
 
 class CartViewState {
   public isInitializing: boolean = false
@@ -11,6 +15,8 @@ const cartViewState = new CartViewState();
 
 export default function Cart() {
   const [isBusy, setBusy] = useState(true);
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const cartState: CartState = useSelector((state: RootState) => state.cart);
 
   const fetchItems = () => {
     setBusy(true);
@@ -19,6 +25,7 @@ export default function Cart() {
       setBusy(false);
       cartViewState.isInitializing = false;
 
+      setCartItems([...cartState.items]);
     }, getRandom(500, 2500));
   };
 
@@ -27,6 +34,8 @@ export default function Cart() {
       return;
     }
     cartViewState.isInitializing = true;
+
+    fetchItems();
   }, []);
 
   return (
@@ -40,7 +49,10 @@ export default function Cart() {
       }
       { !isBusy &&
         <Col className={'cart-view w-full h-full min-h-96'}>
-          <Row className={'cart-footer px-5 w-full'}>
+          <Row>
+          </Row>
+
+          <Row className={'cart-footer mt-5 px-5 w-full'}>
             <button
               className={'add-to-cart-btn w-full select-none rounded-lg bg-amber-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-black shadow-md shadow-amber-500/20 transition-all hover:shadow-lg hover:shadow-amber-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'}
             >
